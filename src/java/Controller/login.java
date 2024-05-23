44e0745856c3c3d898e30af05079c6e4ec1fa27f
Nguyễn Thái Sơn <sonnthe172793@fpt.edu.vn>
2024-05-23 16:05:45 +0700

    login.java
    int new a

--- src/java/Controller/login.java
+++ src/java/Controller/login.java
@@ -30,7 +30,7 @@ public class login extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-
+            int a = 5;
         }
     } 
 
